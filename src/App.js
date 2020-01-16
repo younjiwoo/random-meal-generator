e598299de1d04b7c3ed33494a9db0efbf8535458
Younji Woo <younjiwoo@Younjis-MBP.home>
2020-01-15 23:25:19 -0500

    save before creating the meal api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import MealDisplay from './MealDisplay.js';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ recipe, setRecipe ] = useState(null);
+
+	useEffect(() => {
+		const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+		res
+			.json()
+			.then(res => setRecipe(res))
+			.catch(() => console.log('Something went wrong with the meal DB.'));
+	});
+
+	return (
+		<div className="App">
+			<h3>Feeling hungry?</h3>
+			<h5>Get a random meal by clicking below</h5>
+			<Button variant="contained">Get Meal 🍔</Button>
+			<MealDisplay />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/MealDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MealDisplay.scss';
+
+function MealDisplay() {
+
+    return (
+      <div className="meal-container">
+        HIII
+      </div>
+    );
+  }
+  
+  export default MealDisplay;
