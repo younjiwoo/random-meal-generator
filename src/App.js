0f40b15b40c431760d31365d5a8383f6a5c41349
Younji Woo <younjiwoo@Younjis-MBP.home>
2020-01-22 10:51:27 -0500

    make a call to MealDB to get a random recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import MealDisplay from './MealDisplay.js';
-import './App.css';
+import './App.scss';
 import Button from '@material-ui/core/Button';
 
 function App() {
 	const [ recipe, setRecipe ] = useState(null);
 
-	useEffect(() => {
-		const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-		res
-			.json()
-			.then(res => setRecipe(res))
-			.catch(() => console.log('Something went wrong with the meal DB.'));
-	});
+	async function fetchRecipe() {
+		let res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+		res = await res.json();
+		setRecipe(res.meals[0]);
+		console.log(res.meals[0]);
+	}
+  
+	// useEffect(() => {
+	// 	fetchRecipe();
+	// }, []);
 
 	return (
 		<div className="App">
 			<h3>Feeling hungry?</h3>
 			<h5>Get a random meal by clicking below</h5>
-			<Button variant="contained">Get Meal 🍔</Button>
-			<MealDisplay />
+			<Button variant="contained" onClick={() => fetchRecipe()}>Get Meal 🍔</Button>
+			{
+				recipe &&
+
+					<MealDisplay 
+						data={recipe}
+					/>
+			}
 		</div>
 	);
 }
--- src/MealDisplay.js
+++ src/MealDisplay.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './MealDisplay.scss';
 
-function MealDisplay() {
+function MealDisplay(props) {
+
+    const ingredients = [];
+    for (let i = 1; i <= 30; i++) {
+        if (props.data[`strIngredient${i}`]) {
+            ingredients.push(`${props.data[`strIngredient${i}`]} - ${props.data[`strMeasure${i}`]}`);
+        } else {
+            break; // stop if no more ingredients
+        }
+    }
+
+    const ingredientList = ingredients.map(ingredient => {
+        return <li>{ingredient}</li>
+    })
 
     return (
       <div className="meal-container">
-        HIII
+        <img src={props.data.strMealThumb} alt={`Image of ${props.data.strMeal}`} />
+        <h3 className="blue-font">{props.data.name}</h3>
+        <p><strong>Category: </strong>{props.data.strCategory}</p>
+        <p><strong>Area: </strong>{props.data.strArea}</p>
+        <p><strong>Tags: </strong>{props.data.strTags}</p>
+        <p><strong>Ingredients: </strong></p>
+        <ul>
+            {ingredientList}
+        </ul>
+       
+
       </div>
     );
   }
